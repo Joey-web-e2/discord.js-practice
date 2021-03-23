9c729035c8e3148f1f3165be6cb4045c56c75815
Joey-web-e2 <81253852+Joey-web-e2@users.noreply.github.com>
2021-03-23 13:18:36 -0500

    update index.js
    added some commands aaa

--- index.js
+++ index.js
@@ -2,14 +2,41 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('bot on');
 });
 
 client.login('your-token-goes-here');
 client.on('message', message => {
-	if (message.content === '!ping') {
-	// send back "Pong." to the channel the message was sent in
-	message.channel.send('Pong.');
+	if (message.content === '.discord neu') {
+	message.ch0annel.send('discord.gg/moulberry');
 }
 });
+
+client.login('your-token-goes-here');
+client.on('message', message => {
+	if (message.content === '.discord sba') {
+	message.ch0annel.send('discord.gg/buiscit');
+}
+});
+
+});
+
+client.login('your-token-goes-here');
+client.on('message', message => {
+	if (message.content === '.crashlog') {
+	message.ch0annel.send('C:\user\appdata\roaming\.minecraft\skyclient\crashlogs');
+}
+});
+
+});
+
+client.login('your-token-goes-here');
+client.on('message', message => {
+	if (message.content === '.howtoask') {
+	message.ch0annel.send('**explain your issue** whats happening? ask whats causing it?');
+}
+});
+
+
+
 
